481da798be63ec62e1d8bafb63feefa674ed46b2
Jose Marty <jose.m.marty@gmail.com>
2026-07-16 04:14:16 -0400

    UPD

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,3 @@
-# © 2016 Serpent Consulting Services Pvt. Ltd. (http://www.serpentcs.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sienna Website Extensions",
@@ -8,19 +6,15 @@
     "Jose Marty "
     "Jose Marty",
     "website": "https://martydev.com",
-    "version": "16.0.1.0.1",
+    "version": "18.0",
     "license": "AGPL-3",
     "depends": ["website","product_brand", "website_sale","website_sale_product_brand"],
     "data": [
-        #"security/ir.model.access.csv",
-        #"data/website_menu.xml",
         "views/website.xml",
         "views/website_config.xml",
         "views/product_brand_views.xml",
     ],
     "demo": [
-        #"demo/product_brand_demo.xml",
-        #"demo/product_product_demo.xml",
     ],
     "assets": {        
     },
